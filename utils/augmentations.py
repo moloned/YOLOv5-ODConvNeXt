e701f03a55044e02b8198c71dc9ad6bded888be2
david moloney <moloned@gmail.com>
2025-03-24 17:13:24 +0000

    Update augmentations.py

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -118,8 +118,8 @@ def letterbox(im, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleF
     #top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     #left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
     
-    top, bottom = int(round(dh.item() - 0.1)), int(round(dh.item() + 0.1))
-    left, right = int(round(dw.item() - 0.1)), int(round(dw.item() + 0.1))
+    top, bottom = int(round(dh[0].item() - 0.1)), int(round(dh[0].item() + 0.1))
+    left, right = int(round(dw[0].item() - 0.1)), int(round(dw[0].item() + 0.1))
     
     im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
     return im, ratio, (dw, dh)
